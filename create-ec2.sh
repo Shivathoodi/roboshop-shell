d0ff0b100e5c49c002a2d3693b69f80d8e362f99
shiva <shivathoodi@gmail.com>
2023-08-29 12:59:05 +0530

    ec2

---
+++ create-ec2.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+NAMES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
+INSTANCE_TYPE=t2.micro
+IMAGE_ID=ami-03265a0778a880afb
+SECURITY_GROUP_ID=sg-0f57d02eee766b7c1
+DOMAIN_NAME=shivalearning.online
+
+for i in "${NAMES[@]}"
+do  
+    if [[ $i == "mongodb" || $i == "mysql" ]]
+    then
+        INSTANCE_TYPE="t3.micro"
+    else
+        INSTANCE_TYPE="t2.micro"
+    fi
+    echo "creating $i instance"
+    IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
+    echo "created $i instance: $IP_ADDRESS"
+
+    aws route53 change-resource-record-sets --hosted-zone-id Z07156243JBIM7GZH4N6G --change-batch '
+    {
+            "Changes": [{
+            "Action": "CREATE",
+                        "ResourceRecordSet": {
+                            "Name": "'$i.$DOMAIN_NAME'",
+                            "Type": "A",
+                            "TTL": 300,
+                            "ResourceRecords": [{ "Value": "'$IP_ADDRESS'"}]
+                        }}]
+    }
+    '
+done
